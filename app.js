497633cf27063108e3c219faa7c848ae3cf48597
David <griffithsdavidchris@gmail.com>
2020-11-11 10:06:13 -0500

    Final

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
-/*var search = document.getElementsByClassName("btn");
-    search.addEventListener('click', function(a) {
-        a.preventDefault();
-        var httpRequest = new  XMLHttpRequest();
-        var  url = "superheroes.php";
-        httpRequest.onreadystatechange = getHeroes;
-        httpRequest.open('GET', url);
-        httpRequest.send();
-
-        function getHeroes() {
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-              if (httpRequest.status === 200) {
-                var response = httpRequest.responseText;
-                var result = document.getElementsByClassName("results");
-                results.innerHTML = response;
-                alert(response);
-            } 
-            else {
-            alert('There was a problem with the request.');
-            }
-            }
-            }
-    }); */
-
-   /* $(document).ready(function(){
+$(document).ready(function(){
       const search = document.querySelector('.btn');
-      var result = document.querySelector('#results');
-
-      $(".btn").click(function(){
-        $.ajax('superheroes.php', 'post', {
-          success: function(response)
-          {results.innerHTML = response;}
-       
-          });
-
-      });
-    
-    }); */
-
-    $(document).ready(function(){
-       const search = document.querySelector('.btn');
       var result = document.querySelector('.results');
       $.ajax({
        url: 'superheroes.php',
